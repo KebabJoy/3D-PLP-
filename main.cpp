239b777a094d7dce014b30d8567171b939c61413
Nikas <nik.prohor@gmail.com>
2023-04-06 18:43:37 +0300

    add rank function

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 #include <algorithm>
 #include <nlohmann/json.hpp>
 #include <random>
+#include <set>
 
-#define PALLET_WIDTH 150.f
-#define PALLET_DEPTH 150.f
+#define PALLET_WIDTH 150
+#define PALLET_DEPTH 150
 
 using json = nlohmann::json;
 using namespace std;
@@ -25,10 +26,10 @@ float totalValue { 0.f };
 
 struct Box {
     int id;
-    float sizeX;
-    float sizeY;
-    float sizeZ;
-    float lPerimeter;
+    int sizeX;
+    int sizeY;
+    int sizeZ;
+    int lPerimeter;
     int seedValue;
 };
 
@@ -36,14 +37,26 @@ struct Population {
     vector<int> boxOrderIds;
     vector<int> boxRotation;
     vector<float> fitness;
-    vector<vector<float> > result;
+    vector<vector<int> > result;
 };
 
 struct Solution {
     float totalLPerimeter;
+    set<int> sP;
+    int numP;
+    int rank;
 };
 
-Solution solution { .totalLPerimeter = 0 };
+struct DBLF {
+    int f1;
+    int f2;
+    int f3;
+    int f4;
+    int f5;
+    int f6;
+};
+
+Solution solution { .totalLPerimeter = 0, .numP = 0 };
 
 void printBoxes(const vector<Box>& boxs){
     for (auto box : boxs) {
@@ -51,6 +64,7 @@ void printBoxes(const vector<Box>& boxs){
         cout << "SizeX: " << box.sizeX << endl;
         cout << "SizeY: " << box.sizeY << endl;
         cout << "SizeZ: " << box.sizeZ << endl;
+        cout << "lPerimeter: " << box.lPerimeter << endl;
         cout << endl;
     }
 }
@@ -58,6 +72,16 @@ void printBoxes(const vector<Box>& boxs){
 void printArray(const vector<int>& arr) {
     for (int i = 0; i < arr.size(); ++i) {
         cout << arr[i] << ' ';
+    }
+}
+
+template<class T>
+void print2DArray(const vector<vector<T> > group){
+    for (int i = 0; i < group.size(); ++i) {
+        for (int j = 0; j < group[i].size(); ++j) {
+            cout << group[i][j] << ' ';
+        }
+        cout << endl;
     }
 }
 
@@ -145,40 +169,32 @@ vector<Population> generatePopulation(const vector<Box>& boxes) {
     return population;
 }
 
-struct DBLF {
-    float f1;
-    float f2;
-    float f3;
-    float f4;
-    float f5;
-    float f6;
-};
-
 void evaluate(vector<Population>& population, const vector<Box>& boxes) {
-    cout << population.size() << endl;
-    vector<vector<float> > fitnesses(population.size());
-    vector<vector<float> > result;
+//    cout << population.size() << endl;
+//    vector<vector<float> > fitnesses;
+//    fitnesses.reserve(population.size());
+    vector<vector<int> > result;
+    float occupiedVol, numberBoxes, value;
+    size_t boxSize = boxes.size();
 
-    float containerVol = PALLET_WIDTH * PALLET_WIDTH * 300;
+    int containerVol = PALLET_WIDTH * PALLET_WIDTH * 300;
     for (int i = 0; i < population.size(); ++i) {
         result.clear();
-        vector<DBLF> dblf = { { .f1 = 0.f, .f2 = 0.f,  .f3 = 0.f, .f4 = PALLET_DEPTH, .f5 = PALLET_WIDTH, .f6 = INT32_MAX } };
-        float occupiedVol = 0, numberBoxes = 0, value = 0;
+        vector<DBLF> dblf = { { .f1 = 0, .f2 = 0,  .f3 = 0, .f4 = PALLET_DEPTH, .f5 = PALLET_WIDTH, .f6 = 300 } };
+        occupiedVol = 0, numberBoxes = 0, value = 0;
 
-        for (int boxNumber = 0; boxNumber < boxes.size(); ++boxNumber) {
-            float boxVol = boxes[boxNumber].lPerimeter;
-            float boxValue = boxes[boxNumber].seedValue;
+        for (int boxNumber = 0; boxNumber < boxSize; ++boxNumber) {
+            int boxVol = boxes[boxNumber].lPerimeter;
+            int boxValue = boxes[boxNumber].seedValue;
 
             sort(dblf.begin(), dblf.end(), [](auto lhs, auto rhs) {
                 return lhs.f4 < rhs.f4;
             });
 
             for (int j = 0; j < dblf.size(); j++) {
-
                 DBLF currentPosition = dblf[j];
-                float l, w, h;
-                float spaceVol = currentPosition.f4 * currentPosition.f5 * currentPosition.f6;
-
+                int l, w, h;
+                int spaceVol = currentPosition.f4 * currentPosition.f5 * currentPosition.f6;
                 int rotation = population[i].boxRotation[boxNumber];
 
                 if(rotation == 0) {
@@ -245,16 +261,133 @@ void evaluate(vector<Population>& population, const vector<Box>& boxes) {
                 }
             }
         }
-
         vector<float > fitness = {
                 (occupiedVol / containerVol * 100.f),
-                (numberBoxes / boxes.size() * 100.f),
-                (value / totalValue * 100.f),
+                ((numberBoxes / boxes.size() * 100.f)),
+                ((value / totalValue * 100.f)),
         };
-        fitnesses[i] = fitness;
         population[i].fitness = fitness;
         population[i].result = result;
     }
+}
+
+vector<float> getDominantSolution(const vector<float>& pFitness, const vector<float>& qFitness){
+    int obj = (int)pFitness.size();
+    vector<bool> dominance;
+
+    for (int i = 0; i < obj; ++i) {
+        if (pFitness[i] >= qFitness[i]){
+            dominance.push_back(true);
+        } else{
+            dominance.push_back(false);
+        }
+    }
+
+    if(dominance[0] && dominance[0] == dominance[1] && dominance[1] == dominance[2]){
+        return pFitness;
+    } else if(!dominance[0] && dominance[0] == dominance[1] && dominance[1] == dominance[2]){
+        return qFitness;
+    } else {
+        return {};
+    }
+}
+
+std::vector<vector<float> > calcDrawingDist(const vector<Population>& population){
+    int obj = population[0].fitness.size() - 1;
+    std::vector<vector<float> > dict1;
+    dict1.reserve(population.size());
+    set<int> ranks;
+
+    for (int i = 0; i < population.size(); ++i) {
+        ranks.insert((int)population[i].fitness[3]);
+    }
+
+    std::vector<std::vector<float> > group;
+    group.reserve(population.size());
+
+    for (auto const& rank : ranks) {
+        group.clear();
+
+        for (int groupInd = 0; groupInd < population.size(); ++groupInd) {
+            if ((int)population[groupInd].fitness[3] == rank) {
+                group.push_back(population[groupInd].fitness);
+            }
+        }
+        for (int i = 0; i < group.size(); ++i) {
+            group[i].push_back(0);
+        }
+
+        for (int i = 0; i < obj; i++) {
+            std::vector<std::vector<float> > sortedGroup(group.begin(), group.end());
+            std::sort(sortedGroup.begin(), sortedGroup.end(),
+                      [i](auto const& x, auto const& y) { return x[i] < y[i]; });
+            std::vector<std::vector<float> > list1;
+            list1.reserve(sortedGroup.size());
+
+            for (auto const& item : sortedGroup) {
+                list1.push_back(item);
+            }
+
+            list1[0][4] = 5000;
+            list1.back()[4] = 5000;
+            for (int j = 1; j < list1.size() - 1; j++) {
+                list1[j][4] += (list1[j + 1][i] - list1[j - 1][i]) / 100;
+            }
+        }
+        dict1.insert(dict1.end(), group.begin(), group.end());
+    }
+    return dict1;
+}
+
+void ranK(vector<Population> population) {
+    vector<Solution> solutions(population.size(), { .totalLPerimeter = 0, .numP = 0 });
+    set<int> frontal;
+
+    for (int currentSolutionInd = 0; currentSolutionInd < population.size(); ++currentSolutionInd) { // loop over fitnesses
+        for (int j = 0; j < population.size(); ++j) { // loop over fitnesses
+            if(currentSolutionInd != j){
+                vector<float> dominance = getDominantSolution(population[currentSolutionInd].fitness,population[j].fitness);
+                if (dominance.empty()){
+                    continue;
+                } else if(dominance == population[currentSolutionInd].fitness) {
+                    solutions[currentSolutionInd].sP.insert(j);
+                } else if(dominance == population[j].fitness) {
+                    solutions[currentSolutionInd].numP += 1;
+                }
+            }
+        }
+
+        if(solutions[currentSolutionInd].numP == 0){
+            solutions[currentSolutionInd].rank = 1;
+            population[currentSolutionInd].fitness.push_back(1);
+            frontal.insert(currentSolutionInd);
+        }
+    }
+
+    int i = 1;
+    while(frontal.size() != 0){
+        set<int> sub;
+        for (auto sol : frontal) {
+            for(auto dominatedSolution : solutions[sol].sP){
+                solutions[dominatedSolution].numP -= 1;
+                if(solutions[dominatedSolution].numP == 0) {
+                    solutions[dominatedSolution].rank = i + 1;
+                    population[dominatedSolution].fitness.push_back(i + 1);
+                    sub.insert(dominatedSolution);
+                }
+            }
+        }
+
+        i += 1;
+        frontal = sub;
+    }
+    calcDrawingDist(population);
+//
+//    for key, value in result.items():
+//        population[key]['Rank'] = value[3]
+//        population[key]['CD'] = value[4]
+
+//    return population;
 }
 
 void geneticAlg(const vector<Box>& boxes) {
@@ -268,7 +401,9 @@ void geneticAlg(const vector<Box>& boxes) {
             cout << "DEBUG geneticALG iteration: " << gens << '\n';
 
             evaluate(population, boxes);
+            ranK(population);
         }
+//        printBoxes(boxes);
     }
 }
 
@@ -283,10 +418,10 @@ int main() {
     for (const auto& item : j) {
         Box box;
         box.id =         item["id"].get<int>();
-        box.sizeX =      item["sizeX"].get<float>();
-        box.sizeY =      item["sizeY"].get<float>();
-        box.sizeZ =      item["sizeZ"].get<float>();
-        box.lPerimeter = item["lPerimeter"].get<float>();
+        box.sizeX =      item["sizeX"].get<int>();
+        box.sizeY =      item["sizeY"].get<int>();
+        box.sizeZ =      item["sizeZ"].get<int>();
+        box.lPerimeter = item["lPerimeter"].get<int>();
         box.seedValue  = item["value"].get<int>();
         boxes.push_back(box);
 
